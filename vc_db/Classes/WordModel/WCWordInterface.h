8cb1c94d6c75a775d8747a7aa1ecc1a5913aedc5
齐江涛 <1051747376@qq.com>
2019-05-29 09:36:00 +0800

    add jsonmodel header

--- vc_db/Classes/WordModel/WCWordInterface.h
+++ vc_db/Classes/WordModel/WCWordInterface.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <JSONModel/JSONModel.h>
 
 @protocol WCWordInterface <NSObject, NSCopying>
 
--- vc_db/Classes/WordModel/WCWordModel.h
+++ vc_db/Classes/WordModel/WCWordModel.h
@@ -7,7 +7,6 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <JSONModel/JSONModel.h>
 #import "WCWordInterface.h"
 
 #pragma mark - 版本
